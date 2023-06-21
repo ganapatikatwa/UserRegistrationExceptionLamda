87acef33c2fe22c0966867d6a99d06ae98d9c53a
Ganapati <ganapatasakatwa@gmail.com>
2023-06-21 09:01:37 +0530

    [GK] UC5 Ms Test Password Rule 1

--- MSTest_Registration/UnitTest1.cs
+++ MSTest_Registration/UnitTest1.cs
@@ -77,5 +77,23 @@ namespace MSTest_Registration
             //Assert
             Assert.AreEqual(expected, var);
         }
+        //UC5
+
+        [TestMethod]
+        public void PasswordRule1()
+        {
+            //Arrange
+
+            REGEX_Registration obj4 = new REGEX_Registration();
+
+            bool expected = true;
+            string Password1 = "aksjdhfg";
+
+            //Act
+            bool var = obj4.ValidatePassword1(Password1);
+
+            //Assert
+            Assert.AreEqual(expected, var);
+        }
     }
 }
