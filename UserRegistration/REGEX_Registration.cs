ac0d9c531e629ae1e2c52b95b9d34fe49be530c2
Ganapati <ganapatasakatwa@gmail.com>
2023-06-25 17:15:32 +0530

    [GK] UC6 PasswordRoll 2

--- UserRegistration/REGEX_Registration.cs
+++ UserRegistration/REGEX_Registration.cs
@@ -41,6 +41,24 @@ namespace UserRegistration
             return Regex.IsMatch(sample, PhNumber);
 
         }
+        //UC5
+
+        public static string PasswordRule1 = "^[a-zA-Z]{8}$";
+
+        public bool ValidatePassword1(string sample)
+        {
+            return Regex.IsMatch(sample, PasswordRule1);
+
+        }
+        //UC6
+
+        public static string PasswordRule2 = "(?=.*[A-Z])[a-zA-Z]{8,}$";
+
+        public bool ValidatePassword2(string sample)
+        {
+            return Regex.IsMatch(sample, PasswordRule2);
+
+        }
     }
 }
 
